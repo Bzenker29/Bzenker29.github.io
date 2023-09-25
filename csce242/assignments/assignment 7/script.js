65ca9849f1efa7b842495c2d08d00d024f819925
bradley <btzenker@Bradleys-MacBook-Pro.local>
2023-09-25 11:39:58 -0400

    updating files

--- csce242/assignments/assignment 7/script.js
+++ csce242/assignments/assignment 7/script.js
@@ -42,8 +42,20 @@ const addExerciseTwo = () => {
     document.getElementById("exercise2").style.display = 'block';
 };
 
-const updateMeter = () => {
+let totalFunds = 0; 
 
+const updateMeter = () => {
+    const moneyCount = parseFloat(document.getElementById("money-count").value);
+
+    if (isNaN(moneyCount) || moneyCount < 0) {
+        alert("Donation amount is invalid. Please enter another amount.");
+        return;
+    };
+
+    totalFunds = totalFunds + moneyCount; 
+    const fundGoal = 10000.00; 
+    const amountAdded = (totalFunds / fundGoal) * 100;
+    document.getElementById("counter").style.height = `${Math.min(amountAdded, 100)}%`;
 };
 
 window.onload = () => {
