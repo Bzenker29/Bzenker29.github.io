0e9cf7af945662870d7f656b3d5cbcfb7f686349
bradley <btzenker@Bradleys-MacBook-Pro.local>
2023-10-01 23:58:13 -0400

    adding assignment 8

--- csce242/assignments/assignment 8/script.js
+++ csce242/assignments/assignment 8/script.js
@@ -14,19 +14,69 @@ const updateMeter = () => {
     document.getElementById("counter").style.height = `${Math.min(amountAdded, 100)}%`;
 };
 
-const runningStart = () => [
+document.addEventListener("DOMContentLoaded", function() {
+    const image = document.getElementById("running");
+    const startButton = document.getElementById("start-run");
+    const stopButton = document.getElementById("stop-run");
+    let animationId;
+    let isRunning = false; 
+    let direction = 1; 
 
-]
+    function updateMan() {
+        const leftMargin = parseFloat(getComputedStyle(image).getPropertyValue("--left-margin")) || 0;
 
-const runningStop = () => {
+        if (isRunning) {
+            image.src = "images/running.png";
+        } else {
+            image.src = "images/running_2.png";
+        }
+
+        const newMargin = leftMargin + 5 * direction;
+        image.style.setProperty("--left-margin", newMargin + "px");
+
+        const containerWidth = image.parentElement.clientWidth;
+        if (newMargin <= 0 || newMargin >= containerWidth - image.clientWidth) {
+            direction *= -1; 
+        }
+
+        isRunning = !isRunning; 
+        animationId = requestAnimationFrame(updateMan);
+    }
+
+    function changeImage() {
+        setInterval(() => {
+            isRunning = !isRunning; 
+        }, 10000); 
+    }
+
+    function startAnimation() {
+        if (!animationId) {
+            animationId = requestAnimationFrame(updateMan);
+            startButton.disabled = true;
+            stopButton.disabled = false;
+        }
+    }
+
+    function stopAnimation() {
+        if (animationId) {
+            cancelAnimationFrame(animationId);
+            animationId = null;
+            startButton.disabled = false;
+            stopButton.disabled = true;
+        }
+    }
+
     
-}
+    startButton.addEventListener("click", () => {
+        changeImage(); 
+        startAnimation(); 
+    });
+    stopButton.addEventListener("click", stopAnimation);
+});
+
 
 
 
 window.onload = () => {
     document.getElementById("funds-count").onclick = updateMeter;
-    document.getElementById("start-run").onclick = runningStart;
-    document.getElementById("stop-run").onclick = runningStop;
-
 }
